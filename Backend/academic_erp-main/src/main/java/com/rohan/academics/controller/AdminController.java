ee2c1d848fab6ac737439c6cea49c576a0042d6f
Rohan Sonawane <rohansonawane1818@gmail.com>
2024-11-23 20:33:49 +0530

    In the admin controller added cross-origin to access frontend at 3000 post

--- Backend/academic_erp-main/src/main/java/com/rohan/academics/controller/AdminController.java
+++ Backend/academic_erp-main/src/main/java/com/rohan/academics/controller/AdminController.java
@@ -6,16 +6,14 @@ import com.rohan.academics.service.AdminService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class AdminController {
     private final AdminService adminservice;
 
